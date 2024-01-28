02e5d56bcaa750ab3e40abcee411aa1efe2e67c9
soukimu1223 <soukimu.career1223@gmail.com>
2024-01-29 02:15:10 +0900

    user crud作成

--- testapp/views.py
+++ testapp/views.py
@@ -1,41 +1,69 @@
 from testapp import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from random import randint
+from testapp import db
+from testapp.models.employee import Employee
 
-@app.route('/')
-def index():
-    my_dict = {
-        "insert_something1":'views.pyのinsert_something1部分です',
-        "insert_something2":'views.pyのinsert_something2部分です',
-        'test_titles':['title1','title2','title3','title4','title5']}
-    return render_template('testapp/index.html', my_dict=my_dict)
 
-@app.route('/test')
-def other1():
-    return render_template('testapp/index2.html')
+@app.get('/')
+def employee_list():
+    employees = Employee.query.all()
+    return render_template('testapp/employee_list.html', employees=employees)
 
-@app.get('sampleform')
-@app.post('/sampleform')
-def sampleform():
+@app.get('/add_employee')
+def get_add_employee():
     if request.method == 'GET':
-        return render_template('testapp/sampleform.html')
+        return render_template('testapp/add_employee.html')
+
+@app.post('/add_employee')
+def post_add_employee():
     if request.method == 'POST':
-        hands={'0':'グー', '1':'チョキ', '2':'パー'}
-        janken_mapping = {'draw':'引き分け', 'win':'勝ち', 'lose':'負け'}
-        player_hand_ja = hands[request.form['janken']]
-        player_hand = int(request.form['janken'])
-        enemy_hand = int(randint(0, 2))
-        enemy_hand_ja = hands[str(enemy_hand)]
-        if player_hand == enemy_hand:
-            judgement = 'draw'
-        elif enemy_hand - player_hand == 1 or enemy_hand - player_hand == -2:
-            judgement = 'win'
-        else:
-            judgement = 'lose'
-        print(f'じゃんけん開始。enemy_hand:{enemy_hand}  player_hand:{player_hand} 結果:{judgement}')
-        result = {
-            'enemy_hand_ja':enemy_hand_ja,
-            'player_hand_ja':player_hand_ja,
-            'judgement':janken_mapping[judgement]
-        }
-        return render_template('testapp/janken_result.html', result=result)
+        form_name = request.form.get('name')
+        form_mail = request.form.get('mail')
+        form_is_remote = request.form.get('is_remote', default=False, type=bool)
+        form_department = request.form.get('department')
+        form_year = request.form.get('year', default=0, type=int)
+        
+        employee = Employee(
+            name=form_name,
+            mail=form_mail,
+            is_remote=form_is_remote,
+            department=form_department,
+            year=form_year
+        )
+        db.session.add(employee)
+        db.session.commit()
+        return redirect(url_for('employee_list'))
+    
+
+@app.route('/employees/<int:id>')
+def employee_detail(id):
+    employee = Employee.query.get_or_404(id)
+    return render_template('testapp/employee_detail.html', employee=employee)
+
+
+@app.get('/employees/<int:id>/edit')
+def employee_edit(id):
+    # 編集ページ表示用
+    employee = Employee.query.get(id)
+    return render_template('testapp/employee_edit.html', employee=employee)
+
+@app.post('/employees/<int:id>/update')
+def employee_update(id):
+    employee = Employee.query.get(id)  # 更新するデータをDBから取得
+    employee.name = request.form.get('name')
+    employee.mail = request.form.get('mail')
+    employee.is_remote = request.form.get('is_remote', default=False, type=bool)
+    employee.department = request.form.get('department')
+    employee.year = request.form.get('year', default=0, type=int)
+
+    db.session.merge(employee)
+    db.session.commit()
+    return redirect(url_for('employee_list'))
+
+@app.route('/employees/<int:id>/delete', methods=['POST'])  
+def employee_delete(id):  
+    employee = Employee.query.get(id)   
+    db.session.delete(employee)  
+    db.session.commit()  
+    return redirect(url_for('employee_list'))
