26f291a47aff17cbd17faf1cd1378c018f591ce3
soukimu1223 <soukimu.career1223@gmail.com>
2024-01-29 22:19:08 +0900

    postgresql

--- testapp/config.py
+++ testapp/config.py
@@ -1,3 +1,4 @@
+import os
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///:sample_flask.db'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://localhost/flasknote"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
